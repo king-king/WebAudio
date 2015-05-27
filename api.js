daa28e734ffdee05c32c59b4b927d623a6c538f0
WQ <niuyiwangqun@sohu.com>
2015-05-27 17:43:42 +0800

    优化play

--- api.js
+++ api.js
@@ -6,11 +6,11 @@
         var context = new AudioContext();
         this.autoPlay = false;
         var audio = this;
-        audio.state = 0;// 0:初始化、1：下载中、2：下载完毕正在解码、3：播放中、4：暂停
+        audio.state = 0;// 0:初始化、1：下载中、2：下载完毕正在解码、3：解码完毕、4：播放中、5：暂停
         audio.currentTime = 0;
         audio.play = function () {
-            if ( audio.state != 3 ) {
-                audio.state = 3;
+            if ( audio.state != 4 && audio.state > 2 ) {
+                audio.state = 4;
                 var source = context.createBufferSource();
                 audio.source = source;
                 source.buffer = audio.buffer;
@@ -20,7 +20,7 @@
         };
 
         audio.pause = function () {
-            audio.state = 4;
+            audio.state = 5;
             audio.source && audio.source.stop();
             audio.currentTime = audio.context.currentTime;
         };
@@ -31,12 +31,15 @@
             },
             set : function ( src ) {
                 this._src = src;
+                audio.state = 1;
                 var xhr = new XMLHttpRequest();
                 audio.context = context;
                 xhr.open( "get", src, true );
                 xhr.responseType = 'arraybuffer';
                 xhr.onload = function () {
+                    audio.state = 2;
                     context.decodeAudioData( xhr.response, function ( buffer ) {
+                        audio.state = 3;
                         audio.buffer = buffer;
                         if ( audio.autoPlay ) {
                             var source = context.createBufferSource();
