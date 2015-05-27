2883b324196f6f1a6323ebf9f8bf13ba7384e6c8
WQ <niuyiwangqun@sohu.com>
2015-05-27 17:18:29 +0800

    添加play和pause方法

--- api.js
+++ api.js
@@ -2,29 +2,45 @@
  * Created by WQ on 2015/5/27.
  */
 (function () {
-    //window.WebAudio = function () {
-    //    this.autoPlay = false;
-    //};
     var WebAudio = function () {
+        var context = new AudioContext();
         this.autoPlay = false;
         var audio = this;
+        audio.currentTime = 0;
+        audio.play = function () {
+            var source = context.createBufferSource();
+            audio.source = source;
+            source.buffer = audio.buffer;
+            source.connect( context.destination );
+            source.start( 0, audio.currentTime );
+        };
+
+        audio.pause = function () {
+            audio.source && audio.source.stop();
+            audio.currentTime = audio.context.currentTime;
+        };
+
         Object.defineProperty( audio, "src", {
             get : function () {
                 return this._src;
             },
             set : function ( src ) {
-                console.log( src );
                 this._src = src;
                 var xhr = new XMLHttpRequest();
-                var context = new AudioContext();
+                audio.context = context;
                 xhr.open( "get", src, true );
                 xhr.responseType = 'arraybuffer';
                 xhr.onload = function () {
                     context.decodeAudioData( xhr.response, function ( buffer ) {
-                        var source = context.createBufferSource();
-                        source.buffer = buffer;
-                        source.connect( context.destination );
-                        source.start( 0 );
+                        audio.buffer = buffer;
+                        if ( audio.autoPlay ) {
+                            var source = context.createBufferSource();
+                            source.connect( context.destination );
+                            audio.source = source;
+                            source.buffer = buffer;
+                            source.start( 0 );
+                        }
+                        audio.onload && audio.onload();
                     } )
                 };
                 xhr.send();
